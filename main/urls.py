2b335e065402332eb3cd07160e928a68614b6450
CK Wong <alvinwong7896@gmail.com>
2024-02-20 17:47:19 +0800

    draft homepage

--- main/urls.py
+++ main/urls.py
@@ -19,9 +19,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from portfolio import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
     # path('blog/', include(blog.urls)),
     # path('portfolio/', include(portfolio.urls)),
 ]
