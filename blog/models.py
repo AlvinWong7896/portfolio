e4a06b5f3893a88e5575de2a4e9a21b94bdd9b3d
CK Wong <alvinwong7896@gmail.com>
2024-02-21 11:34:21 +0800

    created the Blog model and added views, html to display all blogs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    created_on = models.DateTimeField(default=timezone.now)
+    message = models.TextField()
